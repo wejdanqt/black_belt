9d269f106f949d50ed1b7efdd6635d4976926f30
wejdan <wejdanqt@gmail.com>
2019-10-05 15:16:14 +0300

    “Express”

---
+++ Desktop/ExpressCarsAndCats/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/static"));
+
+// app.get('/', (request, response) => {
+//    response.send("Hello Express");
+// });
+
+app.listen(8000, () => console.log("listening on port 8000"));
